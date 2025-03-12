6576a70366245a9037c93fff341d8b3d1e467195
saroquepinho <salvadorkrp@gmail.com>
2025-03-12 17:09:09 +0100

    First session exercice

--- Point.py
+++ Point.py
@@ -7,3 +7,10 @@ class Point:
         """
         self.x = x # define x attribute via self.x
         self.y = y # and assign value x to it
+
+# now we need to instantiate it!
+p = Point(1,2) # p is an instance of 1 and 2
+p2 = Point(2,3)
+p4 = Point(4.4, -55)
+print(f"p.x={p.x} and p.y={p.y}")
+print(f"p4.x={p4.x} and p4.y={p4.y}")
