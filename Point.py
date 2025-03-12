0950baf50570a9e1b71820dc73a287b3462b8c05
saroquepinho <salvadorkrp@gmail.com>
2025-03-12 17:04:15 +0100

    First session exercice

---
+++ Point.py
@@ -0,0 +1,9 @@
+class Point:
+    def __init__(self, x, y):
+        """
+        Initiate a Point Project
+        :param x: the x position
+        :param y: the y position
+        """
+        self.x = x # define x attribute via self.x
+        self.y = y # and assign value x to it
